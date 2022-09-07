5032557078a4a03815ad0a789c758faed37de849
Engineering <Engineering@308-10.cls-edu.charlottelatin.org>
2022-09-07 09:48:39 -0400

    push2

--- HotelProject/HotelRoom.java
+++ HotelProject/HotelRoom.java
@@ -1,29 +1,98 @@
 public class HotelRoom{
     public Person guestOne = null;
     public Person guestTwo = null;
+    public List<Person> additionalGuests = new List<Person>();
     public int roomNumber = 0;
+    public int roomCapacity = 0;
 
-
-    public static void main(String[] args){
-        
+    public enum roomTypeData {
+        Normal,
+        Elderly
     }
+    public roomTypeData roomType;
 
-    public HotelRoom(int r){
+    public HotelRoom(int r, int rCapacity, roomTypeData rType){
         roomNumber = r;
+        roomCapacity = rCapacity;
+        roomType = rType;
     }
 
     public Boolean checkIn(Person guest){
-        if (checkCapacity() == 2){
-            System.out.println("Room is full!");
+
+        if (roomCapacity >2){
+            if (guest.age < 60 && roomType == roomTypeData.roomType.Elderly){
+                System.out.println("Guest is to young for the elderly suite!");
+                return false;
+            }
+
+            if (guestOne == null){
+                System.out.println("Added guest to slot 1!");
+                guestOne = guest;
+                return true;
+            }
+            else if (guestTwo == null){
+                System.out.println("Added guest to slot 2!");
+                guestTwo = guest;
+                return true;
+            }
+            else{
+                if (additionalGuests.size() < rCapacity){
+                    additionalGuests.Add(guest);
+                    System.out.println("Added guest to the additional room list!");
+                    return true;
+                }
+                else{
+                    System.out.println("Room is full!");
+                    return false;
+                }
+            }
+
             return false;
         }
-        else if (guestOne == null){
-            System.out.println("Added guest to slot 1!");
-            guestOne = guest;
+
+
+
+        if (roomType == roomTypeData.Normal){
+            if (checkCapacity() == 2){
+                System.out.println("Room is full!");
+                return false;
+            }
+            else if (guestOne == null){
+                System.out.println("Added guest to slot 1!");
+                guestOne = guest;
+                    return true;
+            }
+            else if (guestTwo == null){
+                System.out.println("Added guest to slot 2!");
+                guestTwo = guest;
+                    return true;
+            }
         }
-        else if (guestTwo == null){
-            System.out.println("Added guest to slot 2!");
-            guestTwo = guest;
+        else if (roomType == roomTypeData.Elderly){
+            if (guest.age >= 60){
+                if (checkCapacity() == 2){
+                    System.out.println("Room is full, cannot add elderly guest!");
+                    return false;
+                }
+                else if (guestOne == null){
+                    System.out.println("Added elderly guest to slot 1!");
+                    guestOne = guest;
+                    return true;
+                }
+                else if (guestTwo == null){
+                    System.out.println("Added elderly guest to slot 2!");
+                    guestTwo = guest;
+                    return true;
+                }
+            }
+            else{
+                if (checkCapacity() == 2){
+                    System.out.println("Guest is not eligable for the elderly suite, and the room is full!");
+                    return false;
+                }
+                System.out.println("Guest is too young for the elderly suite!");
+                return false;
+            }
         }
 
         return false;
@@ -53,6 +122,24 @@ public class HotelRoom{
     }
 
     public int checkCapacity(){
+
+        if (roomCapacity > 2){
+            int currentGuestCount = 0;
+
+            if (guestOne != null){
+                currentGuestCount += 1;
+            }
+            if (guestTwo != null){
+                currentGuestCount += 1;
+            }
+
+            currentGuestCount += additionalGuests.size();
+
+            return currentGuestCount;
+        }
+
+
+
         if (guestOne != null && guestTwo != null){
             System.out.println("We have 2 guests in this room!");
             return 2;
--- HotelProject/Primary.java
+++ HotelProject/Primary.java
@@ -1,12 +1,14 @@
 public class Primary{
 
     static HotelRoom room;
+    static HotelRoom elderlyRoom;
 
     public static void main(String[] args){
         Person p1 = new Person(5.4, 25, "John", "Brown");
         Person p2 = new Person(3.6, 34, "Bob", "Blond");
 
-        room = new HotelRoom(0);
+        room = new HotelRoom(0, HotelRoom.roomTypeData.Normal);
+        elderlyRoom = new HotelRoom(0, HotelRoom.roomTypeData.Elderly);
 
         if (room.checkCapacity() == 0){
             room.checkIn(p1);
@@ -19,5 +21,14 @@ public class Primary{
             room.checkOut(p2);
             room.printGuestInfo();
         }
+
+        room.checkIn(p1);
+        room.checkIn(p2);
+        room.checkIn(new Person(4.2, 28, "Joe", "Black"));
+        room.printGuestInfo();
+
+
+        elderlyRoom.checkIn(new Person(4.2, 28, "Joe", "Black"));
+        elderlyRoom.checkIn(new Person(4.2, 65, "Joe", "Black"));
     }
 }
